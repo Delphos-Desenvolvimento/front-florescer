47980b7e8fe6981879067cf6430e410cf07470c7
unknown <megacraftng500@outlook.com>
2025-12-11 14:55:14 -0300

    Correção do sobre

--- src/componnents/page/About.tsx
+++ src/componnents/page/About.tsx
@@ -281,7 +281,7 @@ function About() {
 
       <Container maxWidth="xl">
         {/* Seção Sobre Nós */}
-        <Box id="sobre" sx={{ mb: { xs: 8, md: 12 }, scrollMarginTop: '88px' }}>
+        <Box id="sobre" sx={{ mb: { xs: 8, md: 12 }, scrollMarginTop: '78px' }}>
           <div>
             <Box
               sx={{
