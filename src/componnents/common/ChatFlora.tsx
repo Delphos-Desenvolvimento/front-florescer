0216696b45366d2712ffdacd1888a9189f76e85b
Delphos <delphosdesenvolvimento@gmail.com>
2026-02-10 16:04:29 -0300

    Flora versao moblie estatica

--- src/componnents/common/ChatFlora.tsx
+++ src/componnents/common/ChatFlora.tsx
@@ -114,42 +114,44 @@ const ChatFlora = () => {
                 objectFit: 'contain',
               }}
             />
-            <Box
-              sx={{
-                position: 'absolute',
-                right: -12,
-                top: 0,
-                height: '100%',
-                width: { xs: '56%', md: '50%' },
-                overflow: 'visible',
-                pointerEvents: 'none',
-                zIndex: 2,
-              }}
-            >
+            {!isMobile && (
               <Box
-                component="video"
-                autoPlay
-                loop
-                muted
-                playsInline
-                preload="auto"
                 sx={{
                   position: 'absolute',
-                  top: -6,
-                  left: 0,
-                  width: '100%',
+                  right: -12,
+                  top: 0,
                   height: '100%',
-                  objectFit: 'contain',
-                  transform: 'scale(1.6)',
-                  transformOrigin: 'center',
-                  backgroundColor: 'transparent',
+                  width: { xs: '56%', md: '50%' },
+                  overflow: 'visible',
+                  pointerEvents: 'none',
+                  zIndex: 2,
                 }}
               >
-                <source src="/images/video-sem-fundo-convertido-2222-1-tdgldf.hevc.mp4" type='video/mp4; codecs="hvc1"' />
-                <source src="/images/video-sem-fundo-convertido-2222-1-tdgldf.hevc.mp4" type="video/quicktime" />
-                <source src="/images/video%20sem%20fundo.webm" type="video/webm" />
+                <Box
+                  component="video"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  preload="auto"
+                  sx={{
+                    position: 'absolute',
+                    top: -6,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                    transform: 'scale(1.6)',
+                    transformOrigin: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                >
+                  <source src="/images/video-sem-fundo-convertido.hevc.mp4" type='video/mp4; codecs="hvc1"' />
+                  <source src="/images/video-sem-fundo-convertido.hevc.mp4" type="video/quicktime" />
+                  <source src="/images/video%20sem%20fundo.webm" type="video/webm" />
+                </Box>
               </Box>
-            </Box>
+            )}
           </Box>
         </Box>
       )}
